81d66e925565ee6232ff9b668509e8c924e4df42
Tommy <Tommy>
2017-08-14 08:17:14 -0700

    changed the spans into divs

--- employee.js
+++ employee.js
@@ -167,9 +167,9 @@ var when_done_with_auth_stuff = function() {
       if(editing_x === null  ||  editing_y === null  ||  binding === null)
         return warn();
 
-      const {td, span, collab} = array[editing_y][editing_x];
+      const {td, div, collab} = array[editing_y][editing_x];
       td.removeChild(input);
-      td.appendChild(span);
+      td.appendChild(div);
       assert(binding.collaborativeObject === collab);
       assert(binding.domElement === input);
       binding.unbind();
@@ -186,8 +186,11 @@ var when_done_with_auth_stuff = function() {
     for(let i=0; i<5; ++i) {
       const tr = document.createElement('tr');
       for(let j=0; j<5; ++j) {
-        const span = document.createElement('span');
-        array[i][j].span = span;
+        const div = document.createElement('div');
+        array[i][j].div = div;
+        div.style.height = '20px';
+        div.style.width = '100px';
+        div.style.overflow = 'hidden';
         const td = document.createElement('td');
         array[i][j].td = td;
         td.style.border = '1px solid black';
@@ -195,14 +198,14 @@ var when_done_with_auth_stuff = function() {
         td.style.height = '20px';
         const {collab, x, y} = array[i][j];  // Captured by the closure below
         td.addEventListener('dblclick', function(ev) {
-          td.removeChild(span);
+          td.removeChild(div);
           td.appendChild(input);
           input.focus();
           editing_x = x;
           editing_y = y;
           binding = gapi.drive.realtime.databinding.bindString(collab, input);
         });
-        td.appendChild(span);
+        td.appendChild(div);
         tr.appendChild(td);
       }
       table.appendChild(tr);
@@ -212,7 +215,7 @@ var when_done_with_auth_stuff = function() {
     const update_ui = function() {
       for(let i=0; i<5; ++i) {
         for(let j=0; j<5; ++j) {
-          array[i][j].span.innerText = array[i][j].collab.text;
+          array[i][j].div.innerText = array[i][j].collab.text;
         }
       }
     };
